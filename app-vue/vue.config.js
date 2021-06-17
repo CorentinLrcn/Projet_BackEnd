242529a49cf598405cb161e4935ecad4c10ddda8
CorentinLrcn <corentin.laurencine@ynov.com>
2021-06-17 17:26:14 +0200

    [ADD] All project

---
+++ app-vue/vue.config.js
@@ -0,0 +1,7 @@
+const path = require("path")
+
+module.exports = {
+    outputDir: path.resolve('../app-laravel/public/dist'),
+    publicPath: '/dist',
+    indexPath: path.resolve('../app-laravel/resources/views/index.html')
+}
